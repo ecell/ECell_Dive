8baa05c092df17358ad0159a23408c60e2dad20e
Eliott <eliott.jacopin@laposte.net>
2022-10-27 15:38:53 +0900

    [Update] Documentation for the Serializers
    Major:
    - Documentation for ColorDataSerializer, FluxDataSerializer, GraphDataSerializer.

--- Assets/Scripts/Development/ColorDataSerializer.cs
+++ Assets/Scripts/Development/ColorDataSerializer.cs
@@ -1,17 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 
 namespace ECellDive.CustomEditors
 {
+    /// <summary>
+    /// The struct to store the color of a module identifiable 
+    /// by its ID in a Dive Scene.
+    /// </summary>
     [System.Serializable]
     public struct ColorData
     {
+        /// <summary>
+        /// ID of the module.
+        /// </summary>
         public uint targetGoID;
+
+        /// <summary>
+        /// Color of the module.
+        /// </summary>
         public Color color;
     }
 
+    /// <summary>
+    /// A Scriptable object to generate assets (i.e. serialize) data on 
+    /// about the color of data modules during development time.
+    /// Usefull when we want to save visualization setups and load them
+    /// at runtime.
+    /// </summary>
     public class ColorDataSerializer : ScriptableObject
     {
         public ColorData[] data;
--- Assets/Scripts/Development/GraphDataSerializer.cs
+++ Assets/Scripts/Development/GraphDataSerializer.cs
@@ -4,6 +4,12 @@ using ECellDive.Interfaces;
 
 namespace ECellDive.CustomEditors
 {
+    /// <summary>
+    /// A Scriptable object to generate assets (i.e. serialize) data on 
+    /// of a graph during development time.
+    /// Usefull when we want to save the results of a metabolic network
+    /// (CyJson file) to load and at run time.
+    /// </summary>
     public class GraphDataSerializer : ScriptableObject
     {
         public JObject graphData;
@@ -13,13 +19,17 @@ namespace ECellDive.CustomEditors
         [SerializeReference] public INode[] nodes;
         [SerializeReference] public IEdge[] edges;
 
+        /// <summary>
+        /// Used to initialize the field values of the instance.
+        /// </summary>
+        /// <param name="_graph">The original graph data we want
+        /// to serialize.</param>
         public void Initialize(IGraph _graph)
         {
             name = _graph.name;
             graphData = _graph.graphData;
             jNodes = _graph.jNodes;
             jEdges = _graph.jEdges;
-            Debug.Log(_graph.nodes.Length);
             nodes = _graph.nodes;
             edges = _graph.edges;
         }
