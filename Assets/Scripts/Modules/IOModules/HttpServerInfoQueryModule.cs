cfd4b80ff4fcc069c246e83808144d44ba462d47
Eliott <eliott.jacopin@laposte.net>
2023-06-07 18:52:13 +0900

    [Add] AnswerTalker API to Query remote database for reaction information
    Major:
    - Add new module for this task
    - Add new tag menu attached to this module
    - Reaction information is attached to the reaction identified with its GUID thanks to an info tag.

---
+++ Assets/Scripts/Modules/IOModules/HttpServerInfoQueryModule.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using ECellDive.UI;
+using ECellDive.Utility;
+using ECellDive.Interfaces;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace ECellDive.Modules
+{
+    public class HttpServerInfoQueryModule : HttpServerBaseModule
+    {
+        public OptimizedVertScrollList refTargetDatabaseScrollList;
+
+        public TMP_Text refTargetDatabase;
+        public TMP_InputField refReactionID;
+
+        [SerializeField] private AnimationLoopWrapper animLW;
+        [SerializeField] private ColorFlash colorFlash;
+
+        private CyJsonModule LoadedCyJsonPathway;
+
+        private void Start()
+        {
+            GameObject loadedCyJsonGO = GameObject.FindGameObjectWithTag("CyJsonModule");
+            if (loadedCyJsonGO == null)
+            {
+                LogSystem.AddMessage(LogMessageTypes.Errors,
+                    "[HttpServerInfoQueryModule] There is no active metabolic pathway (CyJson) module detected.");
+                GetComponentInChildren<ColorFlash>().Flash(0);//fail flash
+            }
+            else
+            {
+                LoadedCyJsonPathway = loadedCyJsonGO.GetComponent<CyJsonModule>();
+
+                LogSystem.AddMessage(LogMessageTypes.Trace,
+                    $"[HttpServerInfoQueryModule] Metabolic pathway (CyJson) module detected {LoadedCyJsonPathway.GetName()}");
+            }
+        }
+
+        /// <summary>
+        /// Sets the text value of the database to use for the query.
+        /// </summary>
+        /// <remarks>Used as callback from the editor.</remarks>
+        /// <param name="dataBaseButtonLabel"></param>
+        public void SetTargetDatabase(TextMeshProUGUI dataBaseButtonLabel)
+        {
+            refTargetDatabase.text = dataBaseButtonLabel.text;
+        }
+
+        private void BuildReactionInfoQuery()
+        {
+            string requestURL = AddPagesToURL(new string[] { "reaction_information2", LoadedCyJsonPathway.GetName(), refReactionID.text});
+            requestURL = AddQueriesToURL(requestURL,
+                new string[] { "db_src", "view_name" },
+                new string[] { refTargetDatabase.text, LoadedCyJsonPathway.GetName() });
+
+            StartCoroutine(GetRequest(requestURL));
+        }
+
+        public void QueryReactionInfo()
+        {
+            //TODO: Start Playing animation
+            StartCoroutine(QueryReactionInfoC());
+        }
+
+        private IEnumerator QueryReactionInfoC()
+        {
+            BuildReactionInfoQuery();
+
+            yield return new WaitUntil(isRequestProcessed);
+
+            if (requestData.requestSuccess)
+            {
+                requestData.requestJObject = JObject.Parse(requestData.requestText);
+                string reactionString = requestData.requestJObject["reaction_information"]["REACTION"].Value<string>();
+
+                EdgeGO edge = LoadedCyJsonPathway.DataID_to_DataGO[Convert.ToUInt16(refReactionID.text)].GetComponent<EdgeGO>();
+                edge.InstantiateInfoTag(new Vector2(0.15f, 0.15f), reactionString);
+            }
+            else
+            {
+                //Flash of the fail color
+                GetComponentInChildren<ColorFlash>().Flash(0);
+                LogSystem.AddMessage(LogMessageTypes.Errors,
+                                       $"[HttpServerInfoQueryModule] Request failed with error: {requestData.requestText}");
+            }
+        }
+    }
+}
