7d7d799d7cc1e6a971a40d4be706130f7ff72367
Jacopin Eliott <eliott.jacopin@laposte.net>
2023-10-21 23:00:32 +0900

    [Update] Completed documentation of HttpServerAPICheckModule

--- Assets/Scripts/Modules/IOModules/HttpServerAPICheckModule.cs
+++ Assets/Scripts/Modules/IOModules/HttpServerAPICheckModule.cs
@@ -30,6 +30,12 @@ namespace ECellDive.Modules
         /// </summary>
 		[SerializeField] private ColorFlash colorFlash;
 
+		/// <summary>
+		/// The method to build the request URL for checking the API
+		/// </summary>
+		/// <returns>
+		/// The request URL for checking the API
+		/// </returns>
 		private string BuildCheckAPIRequest()
 		{
 			return AddPagesToURL(new string[] { "apis" });
@@ -46,6 +52,11 @@ namespace ECellDive.Modules
             animLW.PlayLoop("HttpServerAPICheckModule");
         }
 
+		/// <summary>
+		/// The coroutine to request the list of implemented API
+		/// from the server and update which server action modules can
+		/// be used in ECellDive as a result.
+		/// </summary>
 		private IEnumerator CheckAPIC()
 		{
 			StartCoroutine(GetRequest(BuildCheckAPIRequest()));
@@ -64,12 +75,16 @@ namespace ECellDive.Modules
 				List<string> apiCmdNames = requestData.requestJObject["apis"].Select(jv => (string)jv).ToList();
 
 				apiCmdNames.Sort();
+
+				//Get the interactibility manager of the modules menu
 				IInteractibility interactibilityManager = StaticReferencer.Instance.refExternalObjectContainer.GetComponent<GUIManager>().refModulesMenuManager;
 				for(int i = 0; i < interactibilityManager.targetGroup.Length; i++)
 				{
-					bool allAPIImplemented = true;
+					//Get the HttpServerBaseModule component of the module.
+					//This is to get the list of implemented API of the module.
 					HttpServerBaseModule httpServerBaseModule = interactibilityManager.targetGroup[i].GetComponent<GameObjectConstructor>().refPrefab.GetComponent<HttpServerBaseModule>();
 
+					bool allAPIImplemented = true;
 					if (httpServerBaseModule != null)
 					{
 						foreach (string apiCmdName in interactibilityManager.targetGroup[i].GetComponent<GameObjectConstructor>().refPrefab.GetComponent<HttpServerBaseModule>().implementedHttpAPI)
@@ -77,6 +92,11 @@ namespace ECellDive.Modules
 							int idx = apiCmdNames.BinarySearch(apiCmdName);
 							allAPIImplemented &= (idx >= 0 && idx < apiCmdNames.Count);
 						}
+						//Update the interactibility of the button to spawn the module.
+						//TODO: eventually, users might connect to different servers with overlapping API.
+						//In that case, we should check if a module is already allowed to be used by another
+						//server and avoid disabling it if its API is not implemented by the server we are
+						//currently checking.
 						interactibilityManager.ForceSingleInteractibility(i, allAPIImplemented);
 					}
 				}
